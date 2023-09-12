7a2d94166cd05912200736917f1cdc3fd5faa244
hayathine <97364703+hayathine@users.noreply.github.com>
2023-09-13 07:18:51 +0900

    gitignoreから databaseを除く

---
+++ sql_app/databases/database.py
@@ -0,0 +1,51 @@
+from sqlalchemy import create_engine
+from sqlalchemy_utils import database_exists, create_database
+from sqlalchemy.orm import sessionmaker, scoped_session
+from dotenv import load_dotenv
+import os
+from models.models import Base
+
+
+# DB情報
+load_dotenv('.env')
+user_name = os.getenv("DB_USERNAME")
+password = os.getenv("DB_PASSWORD")
+host = os.getenv("DB_HOST")
+database_name = os.getenv("DATABASE_NAME")
+
+SQLALCHEMY_DATABASE_URL = "mysql://%s:%s@%s/%s?charset=utf8" % (
+    user_name,
+    password,
+    host,
+    database_name,
+)
+
+# DB接続
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, 
+    # encoding="utf-8",
+    echo=True
+    )
+
+# セッション作成
+sessionLocal = scoped_session(
+    sessionmaker(autocommit=False, autoflush=False, bind=engine)
+)
+
+# DBが存在しない場合は作成する
+if  not database_exists(engine.url):
+    create_database(engine.url)
+# テーブル作成
+Base.metadata.create_all(bind=engine)
+
+# DB接続用のセッションクラス、インスタンスが作成されるとDBに接続される
+Base.query = sessionLocal.query_property()
+
+class Access_DB:
+    @staticmethod
+    def get_db():
+        db = sessionLocal()
+        try:
+            yield db
+        finally:
+            db.close()
